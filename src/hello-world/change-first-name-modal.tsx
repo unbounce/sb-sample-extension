3edfea6ba874ef9419ba84d44afe1dd2f467c3a8
Maxime Cloutier <maxime.cloutier@unbounce.com>
2021-12-06 10:11:55 -0700

    CN-6218 Add eslint and prettier rules (#6)
    * CN-6218 Add eslint and prettier rules
    
    * Update .prettierrc
    
    Co-authored-by: Brian Burns <brian.burns@unbounce.com>
    
    Co-authored-by: Brian Burns <brian.burns@unbounce.com>

--- src/hello-world/change-first-name-modal.tsx
+++ src/hello-world/change-first-name-modal.tsx
@@ -1,26 +1,25 @@
-import React, { useState } from "config/global-dependencies/react";
-import { Modal, CloseButton, Overlay } from "./styled";
-import { Props } from "./types";
+import React, { useState } from 'config/global-dependencies/react';
 
-export const ChangeFirstNameModal = ({
-  firstName,
-  onUpdate,
-  onClose,
-}: Props) => {
+import { Modal, CloseButton, Overlay } from './styled';
+import { Props } from './types';
+
+export const ChangeFirstNameModal = ({ firstName, onUpdate, onClose }: Props) => {
   const [localFirstName, setLocalFirstName] = useState(firstName);
   return (
     <>
       <Modal>
         <CloseButton onClick={onClose}>x</CloseButton>
-        <label>First Name:</label>
+        <label>
+          <strong>First Name:</strong>
+          <br />
+          <input
+            type="text"
+            value={localFirstName}
+            onChange={({ currentTarget: { value } }) => setLocalFirstName(value)}
+          />
+        </label>
         <br />
-        <input
-          type="text"
-          value={localFirstName}
-          onChange={({ currentTarget: { value } }) => setLocalFirstName(value)}
-        />
-        <br />
-        <button onClick={(e) => onUpdate(localFirstName)}>Save</button>
+        <button onClick={() => onUpdate(localFirstName)}>Save</button>
       </Modal>
       <Overlay />
     </>
