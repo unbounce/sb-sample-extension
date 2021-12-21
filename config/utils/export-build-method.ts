7ff4644a5ac07742f8c3ae11b1befb8115d27e4a
Maxime Cloutier <maxime.cloutier@unbounce.com>
2021-12-21 10:28:15 -0700

    Expose data fetching (#8)

--- config/utils/export-build-method.ts
+++ config/utils/export-build-method.ts
@@ -1,22 +1,28 @@
 export function exportComponent(bundleMethod: any) {
-  (window as any)["ub"] = {
+  (window as any)['ub'] = {
     externalApp: {
-      component: ({ component }: any, Schema: any) =>
-        bundleMethod(component, Schema),
+      component: ({ component }: any, Schema: any) => bundleMethod(component, Schema),
     },
   };
 }
 export function exportSection(bundleMethod: any) {
-  (window as any)["ub"] = {
+  (window as any)['ub'] = {
     externalApp: {
       section: ({ section }: any, Schema: any) => bundleMethod(section, Schema),
     },
   };
 }
 export function exportControl(bundleMethod: any) {
-  (window as any)["ub"] = {
+  (window as any)['ub'] = {
     externalApp: {
       control: () => bundleMethod(),
+    },
+  };
+}
+export function exportHook(hook: any) {
+  (window as any)['ub'] = {
+    externalApp: {
+      hook,
     },
   };
 }
