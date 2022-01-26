e77dca6f7568e30eda32c734a854c1607fdbac04
Brian Burns <brian.burns@unbounce.com>
2022-01-26 09:44:29 -0800

    CN-6538 add schemas to global (#13)
    * CN-6538 Add schemas
    
    * CN-6538 Update SDK dep

--- config/global-dependencies/smart-builder-shared.ts
+++ config/global-dependencies/smart-builder-shared.ts
@@ -1,5 +1,22 @@
 const sbShared = window['sbShared'];
 
-export const { FullScreenModal, CardContainer, CardList, CardListControls, CardsListWrapper, NoResults } = sbShared;
+export const {
+  FullScreenModal,
+  CardContainer,
+  CardList,
+  CardListControls,
+  CardsListWrapper,
+  NoResults,
+  backgroundImageSchema,
+  backgroundSizeSchema,
+  borderRadiusSchema,
+  colorSchema,
+  cssPositionsSchema,
+  pixelAttributeSchema,
+  stylesSchema,
+  textAlignSchema,
+  textDecorationSchema,
+  typographySchema,
+} = sbShared;
 
 export default sbShared;
