73638eb1887282a91cf603f32720b0094b87cc1a
MaxCloutier <maxi.cloutier@gmail.com>
2022-03-29 08:11:43 -0600

    Cleanup typing

--- config/global-dependencies/smart-builder-sdk.ts
+++ config/global-dependencies/smart-builder-sdk.ts
@@ -1,9 +1,3 @@
-// import 'smart-builder-sdk-types'; breaks the build so it needs to be imported this way even if not used.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line
-import type smartBuilderSdkTypes from 'smart-builder-sdk-types';
-
 export const {
   useApiRequest,
   SdkProvider,
