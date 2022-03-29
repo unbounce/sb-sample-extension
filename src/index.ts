34c20d493a85b9fa1f48c7fdf8bbf9f44c6bf639
Maxime Cloutier <maxi.cloutier@gmail.com>
2022-03-29 09:45:27 -0600

    Add more test coverage (#21)
    * Add more test coverage
    
    * Cleanup typing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { registerControl, registerComponent, registerSection } from 'ub-shared';
+import { registerComponent, registerSection } from 'ub-shared';
 
-import { Control } from './control';
 import { Component } from './hello-world';
 import { Template } from './hello-world-section';
 
@@ -8,6 +7,5 @@ import { Template } from './hello-world-section';
  * Feel free to register whatever you need to export.
  * IMPORTANT: Registration ordering is important.
  */
-registerControl(Control);
 registerComponent(Component);
 registerSection(Template);
