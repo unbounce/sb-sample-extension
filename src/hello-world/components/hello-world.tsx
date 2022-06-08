591be59bc1cfffd26e573cccbf46856b2a2c991e
Pablo Arellano <pablo.arellano@unbounce.com>
2022-06-08 16:47:44 -0700

    Add Scripts examples

--- src/hello-world/components/hello-world.tsx
+++ src/hello-world/components/hello-world.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { WithControls, ControlButton, WithStyles } from 'smart-builder-sdk';
+import { WithControls, ControlButton, WithStyles, Script } from 'smart-builder-sdk';
 import { ComponentProps, WithStylesProps } from 'unbounce-smart-builder-sdk-types';
 
 import { ChangeFirstNameModal } from './change-first-name-modal';
 import { Panel } from './control-panel';
+import { getInlineScript, runOnloadMethod } from './script';
 
 export type DataStructure = { firstName: string; lastName: string; styles: { textAlign: string } };
 
-const HelloWorld = ({ data, dispatch, className }: ComponentProps<DataStructure, WithStylesProps>) => {
+const HelloWorld = ({ data, dispatch, className, mode }: ComponentProps<DataStructure, WithStylesProps>) => {
   const { firstName, lastName } = data;
   const [showModal, setShowModal] = useState(false);
 
@@ -15,6 +16,8 @@ const HelloWorld = ({ data, dispatch, className }: ComponentProps<DataStructure,
     dispatch((api) => api.get('firstName').set(newFirstName));
     setShowModal(false);
   };
+
+  const isViewMode = mode.type === 'view';
 
   return (
     <>
@@ -25,6 +28,22 @@ const HelloWorld = ({ data, dispatch, className }: ComponentProps<DataStructure,
         </button>{' '}
         {lastName}
       </div>
+      {/* To test the External Script we are loading momentjs from a CDN(we chose moment just for simplicity), after loading the library the onload method will run. If you see in the example the mode is set to view and we are using condition: isViewMode. To test it in "edit" mode, you can modify mode to edit and condition to true and the alert should show when editing your landing page  */}
+      {isViewMode ? (
+        <Script
+          mode="view"
+          dependencies={[]}
+          externalScript={{
+            src: 'https://rawgit.com/moment/moment/2.2.1/min/moment.min.js',
+            scriptId: 'moment-sample-script',
+            onloadMethod: runOnloadMethod(), //the onLoadMethod is just an alert of the current time formated.
+            condition: isViewMode, // in this example we are using modeView as condition for testing purposes, but you can use any condition as boolean.
+          }}
+        />
+      ) : null}
+      {/* To test the Inline Script go to view mode and check the console in devtools, you will get the log coming from getInlineScript() */}
+      {isViewMode ? <Script mode={mode.type} dependencies={[]} inlineScript={getInlineScript()} /> : null}
+
       {showModal && (
         <ChangeFirstNameModal firstName={firstName} onUpdate={updateFirstName} onClose={() => setShowModal(false)} />
       )}
