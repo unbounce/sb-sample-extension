073bea3a34f4ad0a2f433f43bddc52b3ca30e121
pabloaredu <pablo.arellano@unbounce.com>
2023-09-25 11:33:28 -0700

    cn-7701 remove comment for testing CI workflow

--- sample-extension/src/index.ts
+++ sample-extension/src/index.ts
@@ -6,6 +6,4 @@ import { Component } from './hello-world';
  * Feel free to register whatever you need to export.
  * IMPORTANT: Registration ordering is important.
  */
-
-// Testing CI workflow
 registerComponent(Component);
